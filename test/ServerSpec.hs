e92a5093172484bc72301ba12da785b67b416412
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2021-10-21 16:58:38 +0200

    test(url): Assert redirection works

--- test/ServerSpec.hs
+++ test/ServerSpec.hs
@@ -2,7 +2,7 @@ module ServerSpec where
 
 import App (Env (..))
 import App.Monad (AppEnv)
-import Data.Aeson (ToJSON , encode)
+import Data.Aeson (ToJSON, encode)
 import qualified Data.ByteString as BS
 import Data.ByteString.Lazy.UTF8 (toString)
 import Data.IORef (IORef, newIORef, readIORef, writeIORef)
@@ -11,16 +11,16 @@ import qualified Data.Map as Map
 import Data.String (IsString (fromString))
 import qualified Data.Text as T
 import Domain.Repository (Repository (Repository, findById, save))
+import Domain.TimeProvider (TimeProvider (TimeProvider))
 import Domain.Urls (Url (urlId))
 import qualified Domain.Urls as Urls
 import Endpoints.Model (RequestUrl (RequestUrl), ShortenedUrl (ShortenedUrl))
 import Network.HTTP.Types (hContentType, methodPost)
 import Network.Wai.Test (SResponse)
 import Test.Hspec (Spec, describe, it)
-import Test.Hspec.Wai (ResponseMatcher (matchStatus), WaiSession, get, request, shouldRespondWith, with)
+import Test.Hspec.Wai (ResponseMatcher (matchHeaders, matchStatus), WaiSession, get, request, shouldRespondWith, with, (<:>))
 import UnliftIO (MonadIO (liftIO))
 import UrlShortener (runServer)
-import Domain.TimeProvider (TimeProvider(TimeProvider))
 
 postJson :: (ToJSON a) => BS.ByteString -> a -> WaiSession st SResponse
 postJson path entity = request methodPost path [(hContentType, "application/json")] (encode entity)
@@ -29,8 +29,8 @@ spec :: Spec
 spec = with (fmap runServer env) $ do
   describe "POST /shorten" $ do
     it "responds with shortened URL" $ do
-      _ <- postJson "/shorten" (RequestUrl "http://example.com") `shouldRespondWith` (toMatcher (ShortenedUrl "1L9zO9O")) { matchStatus = 201}
-      get "/1L9zO9O" `shouldRespondWith` toMatcher Urls.Url {urlRaw = "http://example.com", urlId = "1L9zO9O"}
+      _ <- postJson "/shorten" (RequestUrl "http://example.com") `shouldRespondWith` (toMatcher (ShortenedUrl "1L9zO9O")) {matchStatus = 201}
+      get "/1L9zO9O" `shouldRespondWith` 301 {matchHeaders = ["Location" <:> "http://example.com"]}
   where
     toMatcher :: (ToJSON a) => a -> ResponseMatcher
     toMatcher = fromString . toString . encode
