86a02f395a348c05639cf48ecfb744ede57db794
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2021-10-26 17:14:57 +0200

    fix(log): re-add the ConsoleLogger
    where did he go ?

---
+++ src/Infra/ConsoleLogger.hs
@@ -0,0 +1,11 @@
+module Infra.ConsoleLogger where
+
+import UnliftIO (MonadIO (liftIO))
+import Core (Logger(..))
+import Data.Time (getCurrentTime)
+import Data.Time.Format.ISO8601 (iso8601Show)
+consoleLogger :: (MonadIO m) => IO (Logger m)
+consoleLogger = pure $ Logger $ \sev msg -> do
+  currentTime <- liftIO getCurrentTime
+  liftIO $ putStrLn $ "[" <> iso8601Show currentTime <> "] [" <> show sev <> "] " <> show msg
+  pure ()
