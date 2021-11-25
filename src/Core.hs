c1eecb39dc9bc0f3d5b601a212ff10d6ecab3bfa
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2021-11-25 12:54:08 +0100

    chore(modules): Use autoexporter

--- src/Core.hs
+++ src/Core.hs
@@ -1,16 +1,1 @@
-module Core
-  ( module Core.Urls,
-    module Core.Error,
-    module Core.Has,
-    module Core.Repository,
-    module Core.TimeProvider,
-    module Core.Logger,
-  )
-where
-
-import Core.Error
-import Core.Has
-import Core.Logger
-import Core.Repository
-import Core.TimeProvider
-import Core.Urls
+{-# OPTIONS_GHC -F -pgmF autoexporter #-}
