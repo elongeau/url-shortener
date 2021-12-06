c867f001d6ccf7f748bf8c88950ce78647f9141c
Emmanuel LONGEAU <emmanuel.longeau@gmail.com>
2021-12-06 13:29:11 +0100

    chore(test): Move function to an utils module

--- test/ApiSpec.hs
+++ test/ApiSpec.hs
@@ -1,47 +1,10 @@
 module ApiSpec where
 
-import Control.Monad (void)
-import Core (Logger (Logger))
-import Data.Aeson (ToJSON, encode)
-import qualified Data.ByteString as BS
-import Data.ByteString.Lazy.UTF8 (toString)
-import qualified Data.Pool as Pool
-import Data.String (IsString (fromString))
-import Database.MongoDB (access, deleteAll, master)
 import Handlers (RequestUrl (RequestUrl))
 import Handlers.Data (ShortenedUrl (ShortenedUrl))
-import Infra (AppEnv, Env (envDB, envLogger, envPort), loadConfig)
-import Infra.App (Env (Env))
-import Network.HTTP.Types (hContentType, methodPost)
-import Network.Wai (Application)
-import Network.Wai.Handler.Warp (Port, testWithApplication)
-import Network.Wai.Test (SResponse)
-import Test.Hspec (Spec, SpecWith, beforeWith, describe, it)
-import Test.Hspec.Wai (ResponseMatcher (matchHeaders, matchStatus), WaiSession, get, request, shouldRespondWith, withState, (<:>))
-import UrlShortener (mkAppEnv, runAsApplication)
-
-type TestState = (AppEnv, Application)
-withServer :: SpecWith TestState  -> Spec
-withServer = withState runServer . beforeWith cleanDB
-
-cleanDB :: TestState -> IO TestState
-cleanDB state@(Env {..}, _) = do
-  Pool.withResource envDB $ \pipe -> do
-    let run act = access pipe master "url-shortener" act
-    void $ run $ deleteAll "urls" [([], [])]
-  pure state
-
-runServer :: IO (AppEnv, Application)
-runServer = do
-  env <- loadConfig >>= mkAppEnv >>= withNoLogging
-  let app = runAsApplication env
-  newEnv <- testWithApplication (pure app) (withPort env)
-  pure (newEnv, app)
-  where
-    withNoLogging :: AppEnv -> IO AppEnv
-    withNoLogging env = pure $ env {envLogger = Logger \_ _ -> pure ()}
-    withPort :: AppEnv -> Port -> IO AppEnv
-    withPort env port = pure $ env {envPort = port}
+import Test.Hspec (Spec, describe, it)
+import Test.Hspec.Wai (ResponseMatcher (matchHeaders, matchStatus), get, shouldRespondWith, (<:>))
+import TestUtils (withServer, postJson, toMatcher)
 
 spec :: Spec
 spec = withServer $ do
@@ -56,10 +19,3 @@ spec = withServer $ do
       postJson "/shorten" (RequestUrl "http://example.com") `shouldRespondWith` 409
     it "refuses invalid URL" $ do
       postJson "/shorten" (RequestUrl "not-an-url") `shouldRespondWith` "The submitted url is not a valid url" {matchStatus = 400}
-
-postJson :: (ToJSON a) => BS.ByteString -> a -> WaiSession AppEnv SResponse
-postJson path entity = do
-  request methodPost path [(hContentType, "application/json")] (encode entity)
-
-toMatcher :: (ToJSON a) => a -> ResponseMatcher
-toMatcher = fromString . toString . encode
---
+++ test/TestUtils.hs
@@ -0,0 +1,50 @@
+module TestUtils where
+
+
+import Infra (AppEnv,Env(Env, envLogger, envPort, envDB), loadConfig)
+import Network.Wai (Application)
+import Test.Hspec (SpecWith, Spec, beforeWith)
+import qualified Data.Pool as Pool
+import Network.Wai.Handler.Warp (Port, testWithApplication)
+import Data.Aeson ( ToJSON, encode, encode )
+import qualified Data.ByteString as BS
+import Test.Hspec.Wai (WaiSession, request, withState)
+import Network.Wai.Test (SResponse)
+import Test.Hspec.Wai.Matcher (ResponseMatcher)
+import Data.String (fromString)
+import Data.ByteString.Lazy.UTF8 (toString)
+import Network.HTTP.Types (methodPost)
+import Network.HTTP.Types.Header (hContentType)
+import Database.MongoDB (access, master, deleteAll)
+import Control.Monad (void)
+import UrlShortener (mkAppEnv, runAsApplication)
+import Core (Logger(Logger))
+type TestState = (AppEnv, Application)
+withServer :: SpecWith TestState  -> Spec
+withServer = withState runServer . beforeWith cleanDB
+
+cleanDB :: TestState -> IO TestState
+cleanDB state@(Env {..}, _) = do
+  Pool.withResource envDB $ \pipe -> do
+    let run act = access pipe master "url-shortener" act
+    void $ run $ deleteAll "urls" [([], [])]
+  pure state
+
+runServer :: IO (AppEnv, Application)
+runServer = do
+  env <- loadConfig >>= mkAppEnv >>= withNoLogging
+  let app = runAsApplication env
+  newEnv <- testWithApplication (pure app) (withPort env)
+  pure (newEnv, app)
+  where
+    withNoLogging :: AppEnv -> IO AppEnv
+    withNoLogging env = pure $ env {envLogger = Logger \_ _ -> pure ()}
+    withPort :: AppEnv -> Port -> IO AppEnv
+    withPort env port = pure $ env {envPort = port}
+
+postJson :: (ToJSON a) => BS.ByteString -> a -> WaiSession AppEnv SResponse
+postJson path entity = do
+  request methodPost path [(hContentType, "application/json")] (encode entity)
+
+toMatcher :: (ToJSON a) => a -> ResponseMatcher
+toMatcher = fromString . toString . encode
